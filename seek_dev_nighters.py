023e7dede4253e226d71cc2a4f8fdc8960417a0b
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2016-10-06 17:09:37 +0400

    добавлено описание функций

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,38 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+from datetime import datetime
+import pytz
+import requests
 
-def get_midnighters():
-    pass
+
+def load_attempts():
+    pages = 10
+    all_data = []
+    for page in range(pages):
+        parameters = {"page": page+1}
+        r = requests.get(
+            'https://devman.org/api/challenges/solution_attempts/',
+            params=parameters
+        )
+        if r.status_code == 200:
+            all_data.append(r.json()['records'])
+    return all_data
+
+
+def print_midnighters(data):
+    max_hour = 5
+    for record in data:
+        time = datetime.fromtimestamp(record['timestamp'])
+        time_according_tz = pytz.timezone(record['timezone']).localize(time)
+        if time_according_tz.hour < max_hour:
+            print('%s - %s' % (
+                record['username'],
+                time_according_tz.strftime('%H:%M:%S'))
+            )
+    return True
 
 if __name__ == '__main__':
-  pass
+    pages_data = []
+    pages_data = load_attempts()
+    print("Совы:")
+    for records in pages_data:
+        print_midnighters(records)
+
