ef6ceea1a0020ea157bfea4e286939a28ce2ac78
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2016-10-07 18:20:21 +0400

    добавлена функция поиска кол-ва страниц

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -3,10 +3,19 @@ import pytz
 import requests
 
 
-def load_attempts():
-    pages = 10
+def get_pages_count():
+    parameters = {"page": 1}
+    r = requests.get(
+        'https://devman.org/api/challenges/solution_attempts/',
+        params=parameters
+    )
+    count = r.json()['number_of_pages']
+    return count
+
+
+def load_attempts(pages_count):
     all_data = []
-    for page in range(pages):
+    for page in range(pages_count):
         parameters = {"page": page+1}
         r = requests.get(
             'https://devman.org/api/challenges/solution_attempts/',
@@ -20,19 +29,21 @@ def load_attempts():
 def print_midnighters(data):
     max_hour = 5
     for record in data:
-        time = datetime.fromtimestamp(record['timestamp'])
-        time_according_tz = pytz.timezone(record['timezone']).localize(time)
-        if time_according_tz.hour < max_hour:
-            print('%s - %s' % (
-                record['username'],
-                time_according_tz.strftime('%H:%M:%S'))
-            )
+        timestamp = record['timestamp']
+        if timestamp is not None:
+            time = datetime.fromtimestamp(record['timestamp'])
+            time_according_tz = pytz.timezone(record['timezone']).localize(time)
+            if time_according_tz.hour < max_hour:
+                print('%s - %s' % (
+                    record['username'],
+                    time_according_tz.strftime('%H:%M:%S'))
+                )
     return True
 
 if __name__ == '__main__':
     pages_data = []
-    pages_data = load_attempts()
+    count_of_pages = get_pages_count()
+    pages_data = load_attempts(count_of_pages)
     print("Совы:")
     for records in pages_data:
         print_midnighters(records)
-
